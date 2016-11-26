887d6696f591532af9e383bd0b66eb3ddc586cc6
Clark Corey <ccrift@gmail.com>
2016-11-25 18:25:42 -0700

    Adding comments to csv parsing script

--- snowfall-csv.php
+++ snowfall-csv.php
@@ -1,7 +1,15 @@
 <?php
 
-	error_reporting(0);
+	/**
+	* A procedural php script that parses the snowfall data for the given region. It uses a text file from the national weather
+	* service that is real time.
+	* Json data is returned.
+	*/
 
+	error_reporting(0);
+	date_default_timezone_set('Europe/London');
+
+	//Sheets from the NWS are broken into 6 hour increments
 	function getFormatedTime($time){
 		$returnValue = '';
 
@@ -17,21 +25,19 @@
 		return $returnValue;
 	}
 
-
-	date_default_timezone_set('Europe/London');
-
+	//Set some of the local vars
 	$date = date('Ymd');
 	$time = getFormatedTime(date('H'));
 	$region = $_GET['region'];
-
-	$csv = file_get_contents('http://www.nohrsc.noaa.gov/nsa/discussions_text/' . $region . '/snowfall/' . date('Ym') . '/snowfall_' . $date . $time . '_e.txt');
-	$rows = explode("\n", $csv);
-	
 	$featureCollection = [];
 
+	//Get the csv
+	$csv = file_get_contents('http://www.nohrsc.noaa.gov/nsa/discussions_text/' . $region . '/snowfall/' . date('Ym') . '/snowfall_' . $date . $time . '_e.txt');
+	$rows = explode("\n", $csv);
 	$disclaimer = array_shift($rows);
 	$headers = explode("|", array_shift($rows));
 
+	//Go through each row and parse out the data
 	foreach($rows as $row){
 		
 		$station = [];
@@ -52,6 +58,7 @@
 		}
 	}
 
+	//Return json data
 	echo json_encode(
 		[
 			'type'=>'FeatureCollection',
